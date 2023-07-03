8f0f85429a467c842abe6fdabf8ddeac18a77cd1
Vishal Joshi <91890023+VJoshi611@users.noreply.github.com>
2023-07-03 10:11:23 +0530

    181

--- 181. Employees Earning More Than Their Managers.sql
+++ 181. Employees Earning More Than Their Managers.sql
@@ -1,5 +1,7 @@
 181. Employees Earning More Than Their Managers :
 
+-- Level : Easy  
+  
 -- The Employee table holds all employees including their managers. 
 -- Every employee has an Id, and there is also a column for the manager Id.
 
